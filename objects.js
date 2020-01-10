6e5ef581dd02b4f2e1a772e16fb12421f2c76428
Mary Varnado <myvarnado@gmail.com>
2020-01-10 21:58:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,4 +4,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
 playlist [artistName] = songTitle
 return playlist
   }
-function removeFromPlaylist() {}
+function removeFromPlaylist(playlist, artistName) {
+  delete artistName
+  return playlist
+
+}
